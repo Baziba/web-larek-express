1bff010ec26fb6379c8f8b36787e152e3a8f42a2
Baziba <lisanovd@yandex.ru>
2025-11-09 19:35:42 +0300

    fix: разбил файл констант на хелперы

--- backend/src/helpers/error-messages.ts
+++ backend/src/helpers/error-messages.ts
@@ -1,4 +1,4 @@
-const ERROR = {
+const ErrorMessages = {
   DB_CONNECTION: 'Ошибка подключения к базе данных',
   REQUIRED_FIELD: 'Поле \'%f%\' должно быть заполнено',
   INVALID_DATA: 'Данные не прошли валидацию',
@@ -7,16 +7,7 @@ const ERROR = {
   PRODUCT_TITLE_MAX_LENGTH: 'Максимальная длина поля "title" - 30',
   PRODUCT_DUPLICATE: 'Товар с таким наименованием уже существует',
   PRODUCT_NOT_FOUND: 'Товар не найден',
+  UNAUTHORIZED: 'Пользователь не авторизован',
 };
 
-const HTTP_STATUS = {
-  OK: 200,
-  CREATED: 201,
-  BAD_REQUEST: 400,
-  UNAUTHORIZED: 401,
-  NOT_FOUND: 404,
-  CONFLICT: 409,
-  INTERNAL_SERVER_ERROR: 500,
-};
-
-export { ERROR, HTTP_STATUS };
+export default ErrorMessages;
