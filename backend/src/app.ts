d44b2211d7d22804abf121da20a2e89e76f72d17
Baziba <lisanovd@yandex.ru>
2025-11-09 20:02:22 +0300

    fix: подключил логгеры, config, errorHandler

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -3,26 +3,31 @@ import path from 'path';
 import cors from 'cors';
 import mongoose from 'mongoose';
 import { errors } from 'celebrate';
-import { ERROR } from './constants';
-import router from './routes';
 
-const { PORT = 3000 } = process.env;
-const { DB_ADDRESS = 'mongodb://127.0.0.1:27017/weblarek' } = process.env;
+import router from './routes';
+import { requestLogger, errorLogger } from './middlewares/logger';
+import config from './config';
+import errorHandler from './middlewares/error-handler';
+import ErrorMessages from './helpers/error-messages';
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(router);
 
+app.use(requestLogger);
+app.use(router);
+app.use(errorLogger);
 app.use(errors());
+app.use(errorHandler);
 
 app.use(express.static(path.join(__dirname, './public')));
 
-mongoose.connect(DB_ADDRESS).then(() => {
-  app.listen(PORT, () => {
-    console.info(`listening on port ${PORT}`);
+mongoose.connect(config.DB_ADDRESS)
+  .catch((error) => {
+    console.info(ErrorMessages.DB_CONNECTION, error.message);
   });
-}).catch((error) => {
-  console.info(ERROR.DB_CONNECTION, error.message);
+
+app.listen(config.PORT, () => {
+  console.info(`listening on port ${config.PORT}`);
 });
