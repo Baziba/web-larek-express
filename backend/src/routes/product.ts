05ae7d3780a0fa6400a366d5eddb91504fd4b8f7
Baziba <lisanovd@yandex.ru>
2025-11-09 20:00:14 +0300

    feat: добавил валидацию данных в маршрутизатор product

--- backend/src/routes/product.ts
+++ backend/src/routes/product.ts
@@ -1,11 +1,12 @@
 import { Router } from 'express';
 import { getProducts, createProduct, updateProduct, deleteProduct } from '../controllers/products';
+import { validateObjectId, productBodyValidation } from '../helpers/validation-rules';
 
 const router = Router();
 
 router.get('/', getProducts);
-router.post('/', createProduct);
-router.patch('/:id', updateProduct);
-router.delete('/:id', deleteProduct);
+router.post('/', productBodyValidation, createProduct);
+router.patch('/:id', validateObjectId, productBodyValidation, updateProduct);
+router.delete('/:id', validateObjectId, deleteProduct);
 
 export default router;
