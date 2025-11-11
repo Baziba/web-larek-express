c9af73068ff8fb4b7013479fccabba14d5b51498
Baziba <lisanovd@yandex.ru>
2025-11-12 00:10:05 +0300

    feat: маршрутизация и контроллер заказа

--- backend/src/controllers/order.ts
+++ backend/src/controllers/order.ts
@@ -1,8 +1,42 @@
 import { NextFunction, Request, Response } from 'express';
+import { faker } from '@faker-js/faker';
+import Product from '../models/product';
 import HttpCodes from '../helpers/http-codes';
+import BadRequestError from '../errors/bad-request-error';
 
-const createOrder = (req: Request, res: Response, next: NextFunction) => {
-  res.status(HttpCodes.OK).send(req.body);
+const createOrder = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const {
+      payment, email, phone, address, total, items,
+    } = req.body;
+
+    if (!payment || !email || !phone || !address || !total || !items?.length) {
+      return next(new BadRequestError('Обязательные поля не заполнены'));
+    }
+
+    if (!['card', 'online'].includes(payment)) {
+      return next(new BadRequestError('Способ оплаты неверный'));
+    }
+
+    const products = await Product.find({ _id: { $in: items } });
+    if (products.length !== items.length) {
+      return next(new BadRequestError(`Товар с id ${items[0]} не найден`));
+    }
+
+    const totalFromDB = products.reduce((sum, p) => sum + (p.price ?? 0), 0);
+    if (totalFromDB !== total) {
+      return next(new BadRequestError('Сумма заказа неверная'));
+    }
+
+    const orderId = faker.string.uuid();
+
+    return res.status(HttpCodes.CREATED).send({
+      id: orderId,
+      total,
+    });
+  } catch (error) {
+    return next(error);
+  }
 };
 
-export { createOrder };
+export default createOrder;
