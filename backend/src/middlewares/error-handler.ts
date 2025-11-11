a1e3303985c959697bb5fd2bb490894e497513d6
Baziba <lisanovd@yandex.ru>
2025-11-12 00:05:05 +0300

    fix: упростил errorHandler

--- backend/src/middlewares/error-handler.ts
+++ backend/src/middlewares/error-handler.ts
@@ -1,35 +1,11 @@
-import { ErrorRequestHandler, Request, Response, NextFunction } from 'express';
-import BadRequestError from '../errors/bad-request-error';
-import ConflictError from '../errors/conflict-error';
-import InternalServerError from '../errors/internal-server-error';
-import NotFoundError from '../errors/not-found-error';
-import ErrorMessages from '../helpers/error-messages';
+import { ErrorRequestHandler } from 'express';
 import HttpCodes from '../helpers/http-codes';
+import ErrorMessages from '../helpers/error-messages';
 
-interface CustomError extends Error {
-  statusCode: number;
-}
-
-const errorHandler: ErrorRequestHandler = (
-  err: CustomError,
-  _req: Request,
-  res: Response,
-  next: NextFunction,
-) => {
-  let statusCode = HttpCodes.INTERNAL_SERVER_ERROR;
-  let message = ErrorMessages.INTERNAL_SERVER_ERROR;
-
-  if (
-    err instanceof BadRequestError
-    || err instanceof ConflictError
-    || err instanceof InternalServerError
-    || err instanceof NotFoundError
-  ) {
-    statusCode = err.statusCode;
-    message = err.message;
-  }
-
-  res.status(statusCode).json({ message });
+const errorHandler: ErrorRequestHandler = (err, _req, res, next) => {
+  const statusCode = err.statusCode || HttpCodes.INTERNAL_SERVER_ERROR;
+  const message = statusCode === 500 ? ErrorMessages.INTERNAL_SERVER_ERROR : err.message;
+  res.status(statusCode).send({ message });
   next();
 };
 
